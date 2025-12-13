aa74bada34f79b6dc2cf5d85a9e3ace03e21934d
Evgen <evgeshkarach@gmail.com>
2025-12-13 17:50:43 +0400

    feat(auth): реализовать модели User и Team
    - реализовал кастомную модель User для авторизации с email, оставив только нужные поля
    - реализовал модель Team с полем creator для создания админом множества команд
    - обновил settings.py

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Название команды')
+    description = models.TextField(blank=True, verbose_name='Описание команды')
+    creator = models.ForeignKey('users.UserWithEmail', on_delete=models.PROTECT, related_name='created_teams',
+                                verbose_name='Создатель команды')
+
+    class Meta:
+        verbose_name = 'Команда'
+        verbose_name_plural = 'Команды'
+        ordering = ('name', 'creator')
+
+    def __str__(self):
+        return self.name
--- users/models.py
+++ users/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+from teams.models import Team
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, first_name, last_name, password, **extra_fields):
+        if not email:
+            raise ValueError('Email должен быть обязательно указан')
+        if not first_name:
+            raise ValueError('Имя пользователя должно быть обязательно указано')
+        if not last_name:
+            raise ValueError('Фамилия пользователя должна быть обязательно указана')
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
+
+    def create_superuser(self, email, first_name, last_name, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', UserWithEmail.Role.ADMIN)
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Администратор должен иметь поле is_superuser=True')
+        if extra_fields.get('role') != UserWithEmail.Role.ADMIN:
+            raise ValueError('Администратор должен иметь поле role="ADMIN"')
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
+
+
+class UserWithEmail(AbstractBaseUser, PermissionsMixin):
+    class Role(models.TextChoices):
+        USER = 'user', 'пользователь'
+        MANAGER = 'manager', 'менеджер'
+        ADMIN = 'admin', 'администратор'
+
+    email = models.EmailField(max_length=255, unique=True, db_index=True, verbose_name="Email")
+    first_name = models.CharField(max_length=255, verbose_name="Имя")
+    last_name = models.CharField(max_length=255, verbose_name='Фамилия')
+    role = models.CharField(max_length=7, choices=Role.choices, default=Role.USER, verbose_name='Роль')
+    birthday = models.DateField(blank=True, null=True, verbose_name='Дата рождения')
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, blank=True, null=True, related_name='members',
+                             verbose_name='Команда')
+    is_active = models.BooleanField(default=True, verbose_name='Активность')
+    is_superuser = models.BooleanField(default=False, verbose_name='Админ')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации')
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['email', '-created_at']
+
+    def __str__(self):
+        return f'{self.email} ({self.last_name} {self.first_name})'
