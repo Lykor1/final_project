b40e3d822d364447c5f573fa48307f5c5737addb
Evgen <evgeshkarach@gmail.com>
2025-12-13 18:03:12 +0400

    feat(auth): обновил админ-панель
    - добавил модель User в админ-панель
    - добавил модель Team в админ-панель

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Team
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creator')
+    search_fields = ('name', 'creator')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    model = User
+
+    list_display = (
+        'email',
+        'first_name',
+        'last_name',
+        'role',
+        'team'
+    )
+    search_fields = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'birthday')}),
+        ('Work info', {'fields': ('role', 'team')}),
+        ('Permission', {'fields': ('is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('created_at', 'last_login')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'first_name', 'last_name',
+                'is_active'
+            )
+        }),
+    )
