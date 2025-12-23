336ddf8f9d16a2a90a44814cc2428e13ac19c447
Evgen <evgeshkarach@gmail.com>
2025-12-23 10:45:10 +0400

    feat(task): реализовано представление для обновления задачи
    - реализовано представление для обновления
    - реализована сервисная функция для обновления

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import (
-    CreateAPIView
+    CreateAPIView,
+    UpdateAPIView
 )
 from rest_framework.permissions import IsAdminUser
 
 from teams.models import Team
-from .serializers import TaskCreateSerializer
+from .models import Task
+from .serializers import TaskCreateSerializer, TaskUpdateSerializer
 from .services import TaskService
 
 
@@ -21,5 +23,24 @@ class TaskCreateView(CreateAPIView):
         self.instance = TaskService.create_task(
             created_by=self.request.user,
             team=team,
+            **serializer.validated_data
+        )
+
+
+class TaskUpdateView(UpdateAPIView):
+    """
+    Обновление задачи
+    """
+    permission_classes = (IsAdminUser,)
+    serializer_class = TaskUpdateSerializer
+    queryset = Task.objects.all()
+
+    def get_queryset(self):
+        return Task.objects.filter(created_by=self.request.user)
+
+    def perform_update(self, serializer):
+        task = self.get_object()
+        self.instance = TaskService.update_task(
+            task=task,
             **serializer.validated_data
         )
