eebbd1f8a3a86d0dcb78a547b42d21462e567bad
Evgen <evgeshkarach@gmail.com>
2025-12-25 16:54:33 +0400

    feat(task): модель Comment добавлена в админ-панель

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Task
+from .models import Task, Comment
 
 
 @admin.register(Task)
@@ -8,3 +8,9 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'deadline', 'status', 'team', 'created_by', 'assigned_to')
     list_editable = ('status',)
     search_fields = ('title', 'assigned_to', 'created_by')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('task', 'author', 'text', 'created_at')
+    search_fields = ('author', 'task')
