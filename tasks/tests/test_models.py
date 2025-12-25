bf11610d82011ae7a84a434a49f1d8c32ec8772b
Evgen <evgeshkarach@gmail.com>
2025-12-25 09:41:24 +0400

    test(task): созданы тесты для модели Comment

--- tasks/tests/test_models.py
+++ tasks/tests/test_models.py
@@ -1,8 +1,9 @@
 import pytest
 from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from django.utils import timezone
 
-from tasks.models import Task
+from tasks.models import Task, Comment
 
 
 @pytest.mark.models
@@ -88,3 +89,64 @@ class TestTaskModel:
         """
         fields = Task._meta.ordering
         assert fields == ['created_by', '-created_at']
+
+
+@pytest.mark.models
+@pytest.mark.django_db
+class TestCommentModel:
+    @pytest.fixture(autouse=True)
+    def setup(self, create_user, create_superuser, create_team, team_data, user_data, admin_user_data, create_task,
+              task_data):
+        self.admin = create_superuser(**admin_user_data)
+        self.user = create_user(**user_data)
+        self.team = create_team(creator=self.admin, **team_data)
+        self.task = create_task(created_by=self.admin, assigned_to=self.user, team=self.team, **task_data)
+
+    def test_create_comment_success(self):
+        """
+        Тест на успешное создание комментария
+        """
+        comment = Comment.objects.create(
+            task=self.task,
+            author=self.admin,
+            text='Hello World!'
+        )
+        assert comment.pk is not None
+        assert Comment.objects.count() == 1
+        assert comment.text == 'Hello World!'
+        assert comment.author == self.admin
+
+    @pytest.mark.parametrize(
+        'field',
+        [
+            'task',
+            'author',
+            'text'
+        ]
+    )
+    def test_create_comment_without_required_fields(self, field):
+        """
+        Тест на создание комментария без связей
+        """
+        data = {
+            'task': self.task,
+            'author': self.admin,
+            'text': 'Hello World!'
+        }
+        data[field] = None
+        with pytest.raises(IntegrityError):
+            Comment.objects.create(**data)
+
+    def test_str_representation(self):
+        """
+        Тест на __str__
+        """
+        comment = Comment.objects.create(task=self.task, author=self.admin, text='Hello World!')
+        assert str(comment) == f'{self.admin}({self.task}): Hello World!'
+
+    def test_default_ordering(self):
+        """
+        Тест на сортировку
+        """
+        fields = Comment._meta.ordering
+        assert fields == ['-created_at', 'task']
