60548de5aaae5e9c534b17b09a42dd9bb4b4d1b6
Evgen <evgeshkarach@gmail.com>
2025-12-30 17:54:07 +0400

    feat(evaluation): реализован сериализатор для создания оценки

---
+++ evaluations/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Evaluation
+
+
+class EvaluationCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Evaluation
+        fields = ('rank',)
+        read_only_fields = fields
