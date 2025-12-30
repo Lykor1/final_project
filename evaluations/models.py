4e37833fb19419948613eea225fb9540f4a38008
Evgen <evgeshkarach@gmail.com>
2025-12-30 17:10:07 +0400

    feat(evaluation): создана модель для оценок

--- evaluations/models.py
+++ evaluations/models.py
@@ -1,3 +1,20 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from tasks.models import Task
+
+
+class Evaluation(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, db_index=True, related_name='task_evaluation',
+                             verbose_name='Задача')
+    rank = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],
+                                            verbose_name='Оценка')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата оценки')
+
+    class Meta:
+        verbose_name = 'Оценка'
+        verbose_name_plural = 'Оценки'
+        ordering = ['task']
+
+    def __str__(self):
+        return f'{self.task.title} ({self.task.assigned_to}): {self.rank}'
