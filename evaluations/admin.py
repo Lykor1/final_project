2fda0e3a4c5016574ef0f098df247a594b31c9f7
Evgen <evgeshkarach@gmail.com>
2026-01-01 11:18:59 +0400

    feat(evaluation): модель оценок добавлена в админ-панель

--- evaluations/admin.py
+++ evaluations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Evaluation
+
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('rank', 'task', 'created_at')
