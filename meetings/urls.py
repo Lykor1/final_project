191626cff1dbb5262728b297f81c1557344fae95
Evgen <evgeshkarach@gmail.com>
2026-01-10 13:56:19 +0400

    feat(meeting): реализовано представление для удаления встречи

--- meetings/urls.py
+++ meetings/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'meetings'
 urlpatterns = [
     path('create/', views.MeetingCreateView.as_view(), name='create'),
+    path('<int:pk>/', views.MeetingDeleteView.as_view(), name='delete'),
 ]
--- meetings/views.py
+++ meetings/views.py
@@ -1,8 +1,9 @@
 from rest_framework.exceptions import ValidationError as DRFValidationError
 from django.core.exceptions import ValidationError
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAdminUser
 
+from .models import Meeting
 from .serializers import MeetingCreateSerializer
 from .services import MeetingService
 
@@ -23,3 +24,14 @@ class MeetingCreateView(CreateAPIView):
             serializer.instance = meeting
         except ValidationError as e:
             raise DRFValidationError(e.message_dict)
+
+
+class MeetingDeleteView(DestroyAPIView):
+    """
+    Удаление встречи
+    """
+    permission_classes = (IsAdminUser,)
+    queryset = Meeting.objects.all()
+
+    def get_queryset(self):
+        return Meeting.objects.filter(creator=self.request.user)
